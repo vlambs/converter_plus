cc31fdf17b8ff06f83b6e82a9810d56a58c13f02
vlambs <ryobudev@gmail.com>
2023-08-18 22:25:53 +0200

    API calls for the currencies combobox values and conversion computing

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 import ttkbootstrap as ttk
+import requests
 
 # ~~ Consts ~~
 CURRENCY_UNITS_LIST=['USD', 'EUR','GBP']
-EXCHANGE_RATES = {
-    'EUR to USD': 1.10,
-    'EUR to GBP': 0.86,
-    'USD to EUR': 0.91,
-    'USD to GBP': 0.79,
-    'GBP to EUR': 1.16,
-    'GBP to USD': 1.27
-}
+
 TEMPERATURE_UNITS_LIST=['Celsius', 'Fahrenheit', 'Kelvin']
 TEMPERATURE_CONVERSION_FUNCTIONS = {
     ('Celsius', 'Fahrenheit'): lambda celsius: celsius * 9/5 + 32,
@@ -52,55 +46,68 @@ def swap_distances():
     combobox_distance_from.current(combobox_distance_to.current())
     combobox_distance_to.current(temp)
 
+# ~~ Methods ~~ 
+def get_currencies(event=None):
+    data = requests.get('https://api.exchangerate.host/symbols').json()
+    currency_codes = list(data["symbols"].keys())
+    combobox_currency_from['values'] = currency_codes
+    combobox_currency_to['values'] = currency_codes
 
-def convert_currency():
+def convert_currency_async():
     if len(input_currency_value.get()) > 0: 
+        # Retrieve ComboBox values ​​and currency to convert
         from_currency = combobox_currency_from_value.get()
         to_currency = combobox_currency_to_value.get()
         amount = float(input_currency_value.get())
         converted_value = converted_currency_value.get()
 
-        conversion_pair = f'{from_currency} to {to_currency}'
-        exchange_rate = EXCHANGE_RATES.get(conversion_pair)
-        if exchange_rate is not None:
-            converted_amount = amount * exchange_rate
-            converted_value = f'{converted_amount:.2f} {to_currency}'
+        # Calling a currency conversion API
+        url = f'https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}'
+        data = requests.get(url).json()
+        is_success_value = data["success"]
+
+        # Checking API call status
+        if is_success_value:
+            # Setting the converted value to display it
+            converted_value = f'{data["result"]:.2f} {to_currency}'
         else:
-            converted_value = 'Conversion not supported'
-    else:
-        converted_value = 'No input amount'
-    converted_currency_value.set(converted_value)
+            converted_value = 'Erreur lors de la conversion'
+        converted_currency_value.set(converted_value)
 
 def convert_temperature():
-    # Récupérer les valeurs des ComboBox et la température à convertir
-    from_temperature = combobox_temperature_from_value.get()
-    to_temperature = combobox_temperature_to_value.get()
-    temperature = float(input_temperature_value.get())
+    if len(input_temperature_value.get()) > 0: 
+        # Retrieve ComboBox values ​​and temperature to convert
+        from_temperature = combobox_temperature_from_value.get()
+        to_temperature = combobox_temperature_to_value.get()
+        temperature = float(input_temperature_value.get())
 
-    # Identifier la paire de conversion et récupérer la fonction de conversion
-    conversion_pair = (from_temperature, to_temperature)
-    conversion_function = TEMPERATURE_CONVERSION_FUNCTIONS.get(conversion_pair)
-    converted_value = converted_temperature_value.get()
+        # Identify the conversion pair and retrieve the conversion function
+        conversion_pair = (from_temperature, to_temperature)
+        conversion_function = TEMPERATURE_CONVERSION_FUNCTIONS.get(conversion_pair)
+        converted_value = converted_temperature_value.get()
 
-    if conversion_function is not None:
-        # Effectuer la conversion en appelant la fonction de conversion
-        converted_temperature = conversion_function(temperature)
-        converted_value = f'{converted_temperature:.2f} {to_temperature}'
-    else:
-        converted_value = 'Conversion non prise en charge'
-    converted_temperature_value.set(converted_value)
-
+        if conversion_function is not None:
+            # Perform the conversion by calling the convert function
+            converted_temperature = conversion_function(temperature)
+            converted_value = f'{converted_temperature:.2f} {to_temperature}'
+        else:
+            converted_value = 'Conversion non prise en charge'
+        converted_temperature_value.set(converted_value)
 
 def convert_distance():
     if len(input_distance_value.get()) > 0: 
+        # Retrieve ComboBox values ​​and distance to convert
         from_distance = combobox_distance_from_value.get()
         to_distance = combobox_distance_to_value.get()
         amount = float(input_distance_value.get())
         converted_value = converted_distance_value.get()
 
+        # Identify the conversion pair and retrieve the conversion function
         conversion_pair = f'{from_distance} to {to_distance}'
         exchange_rate = DISTANCE_CONVERSION.get(conversion_pair)
+
         if exchange_rate is not None:
+            # Perform the conversion by calling the convert function
             converted_amount = amount * exchange_rate
             converted_value = f'{converted_amount:.2f} {to_distance}'
         else:
@@ -128,6 +135,7 @@ header.pack(expand=True)
 # tabs
 tab_control = ttk.Notebook(window)
 tab_currency = ttk.Frame(tab_control)
+tab_currency.bind("<Map>", get_currencies)
 tab_temperature = ttk.Frame(tab_control)
 tab_distance = ttk.Frame(tab_control)
 
@@ -137,8 +145,8 @@ tab_control.add(tab_distance, text='Distance')
 
 tab_control.pack(expand=True, fill='both')
 
-# currency tab
-# 1) header with the currency types
+# CURRENCY TAB
+#   - 1) header with the currency types
 header_currency = ttk.Frame(tab_currency)
 combobox_currency_from_value = ttk.StringVar()
 combobox_currency_to_value = ttk.StringVar()
@@ -153,14 +161,14 @@ button_swap_currencies.pack(side='left', padx=10)
 combobox_currency_to.pack(side='left', padx=10, expand=True, fill=tk.BOTH)
 header_currency.pack(fill=tk.X, expand=True, )
 
-# 2) user entry
+#   - 2) user entry
 input_currency_frame = ttk.Frame(tab_currency)
 input_currency_value = ttk.StringVar()
 input_currency = ttk.Entry(input_currency_frame, textvariable=input_currency_value).pack(side='left', padx = 10, fill=tk.BOTH, expand=True)
-button_convert_currency = ttk.Button(input_currency_frame, text='Convert', command=convert_currency).pack(side='left', padx=10)
+button_convert_currency = ttk.Button(input_currency_frame, text='Convert', command=convert_currency_async).pack(side='left', padx=10)
 input_currency_frame.pack(expand=True, fill=tk.X)
 
-# 3) converted value
+#   - 3) converted value
 converted_currency_value = ttk.StringVar()
 ttk.Label(
     tab_currency, 
@@ -168,8 +176,8 @@ ttk.Label(
     font='Calibri 25 bold'
 ).pack(anchor=tk.CENTER)
 
-# temperature tab
-# 1) header with the temperature types
+# TEMPERATURE TAB
+#   - 1) header with the temperature types
 header_temperature = ttk.Frame(tab_temperature)
 combobox_temperature_from_value = ttk.StringVar()
 combobox_temperature_to_value = ttk.StringVar()
@@ -183,14 +191,14 @@ button_swap_currencies.pack(side='left', padx=10)
 combobox_temperature_to.pack(side='left', padx=10, expand=True, fill=tk.BOTH)
 header_temperature.pack(fill=tk.X, expand=True, )
 
-# 2) user entry
+#   - 2) user entry
 input_temperature_frame = ttk.Frame(tab_temperature)
 input_temperature_value = ttk.StringVar()
 input_temperature = ttk.Entry(input_temperature_frame, textvariable=input_temperature_value).pack(side='left', padx = 10, fill=tk.BOTH, expand=True)
 button_convert_temperature = ttk.Button(input_temperature_frame, text='Convert', command=convert_temperature).pack(side='left', padx=10)
 input_temperature_frame.pack(expand=True, fill=tk.X)
 
-# 3) converted value
+#   - 3) converted value
 converted_temperature_value = ttk.StringVar()
 ttk.Label(
     tab_temperature, 
@@ -199,8 +207,8 @@ ttk.Label(
 ).pack(anchor=tk.CENTER)
 
 
-# distance tab
-# 1) header with the distance types
+# DISTANCE TAB
+#  - 1) header with the distance types
 header_distance = ttk.Frame(tab_distance)
 combobox_distance_from_value = ttk.StringVar()
 combobox_distance_to_value = ttk.StringVar()
@@ -214,14 +222,14 @@ button_swap_currencies.pack(side='left', padx=10)
 combobox_distance_to.pack(side='left', padx=10, expand=True, fill=tk.BOTH)
 header_distance.pack(fill=tk.X, expand=True, )
 
-# 2) user entry
+#  - 2) user entry
 input_distance_frame = ttk.Frame(tab_distance)
 input_distance_value = ttk.StringVar()
 input_distance = ttk.Entry(input_distance_frame, textvariable=input_distance_value).pack(side='left', padx = 10, fill=tk.BOTH, expand=True)
 button_convert_distance = ttk.Button(input_distance_frame, text='Convert', command=convert_distance).pack(side='left', padx=10)
 input_distance_frame.pack(expand=True, fill=tk.X)
 
-# 3) converted value
+#  - 3) converted value
 converted_distance_value = ttk.StringVar()
 ttk.Label(
     tab_distance, 
